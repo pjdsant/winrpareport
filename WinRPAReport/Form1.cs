41c012fcc4e22b402a2e5edd390a1cbd403095bd
pjdsant@gmail.com <pjdsant@gmail.com>
2019-08-13 19:05:35 -0300

    First Src commit

---
+++ WinRPAReport/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinRPAReport.Resource;
+
+namespace WinRPAReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryManager regman = new RegistryManager();
+
+                lblClicks.Text = regman.ReadRegistry();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblClicks.Text = "0";
+        }
+    }
+}
---
+++ WinRPAReport/Resource/RegistryManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+
+namespace WinRPAReport.Resource
+{
+    public class RegistryManager
+    {
+        public string ReadRegistry()
+        {
+            var result = "";
+
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\PJSIT");
+
+            if (key != null)
+            {
+                result = key.GetValue("Clicks").ToString();
+            }
+
+            return result;
+        }
+    }
+}
